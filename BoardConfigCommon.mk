4f5441a55475c92932b5f85ed9396b7c0a8de3e1
ShirokaneShizuku <tut4n0ta5h10n@tutanota.com>
2024-05-19 21:49:07 +0800

    sm8150-common: Have a Fantasia DerpFest Now!
    * At least we need a Limbo ROM.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,4 @@
-
+#
 # Copyright (C) 2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -138,7 +138,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/derp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
