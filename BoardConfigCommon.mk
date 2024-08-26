f99d39a0e3b6297226501dbab86672e17233fde5
LuK1337 <priv.luk@gmail.com>
2024-08-27 00:00:55 +0200

    sm8150-common: Switch to common device_framework_matrix.xml
    Change-Id: Idc99a59a377bb3284634a62c0a1ae545bcd4e1e2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,7 +119,8 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/sony/vintf/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
