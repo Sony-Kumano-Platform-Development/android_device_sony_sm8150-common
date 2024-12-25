57d307979fc01a3ef23f7aa58e2151a95f81beb8
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm8150-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- common.mk
+++ common.mk
@@ -44,6 +44,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    NcmTetheringOverlay \
     SonyKumanoFrameworksResCommon \
     SonyKumanoSettingsProviderOverlayCommon \
     SonyKumanoSettingsResCommon \
