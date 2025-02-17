a66845ba328d1d8ee8fae13e89df3f0d865e2ac5
LuK1337 <priv.luk@gmail.com>
2025-02-17 23:07:57 +0100

    sm8150-common: Migrate to avbtool --set_verification_disabled_flag
    Change-Id: Ib73f4d97a923b25e25848b24a3554cdd4de1a5ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_verification_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 
 # WiFi
