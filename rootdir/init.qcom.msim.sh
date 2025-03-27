bb41dd71d86afad80109effb447addff801c0b98
kyasu <kyasu.dev@gmail.com>
2025-03-27 09:08:51 +0900

    sm8150-common: Set ro.boot.product.hardware.sku during early-init
    Change-Id: Ic63e456436075128a9019dce9760795ca1c1fe74

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -1,17 +1,17 @@
 #!/vendor/bin/sh
 
-model=$(grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I') 2> /dev/null
+model=$(grep -aim1 'model:' /dev/block/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I') 2> /dev/null
 
 # Desperate measures
 if [ "$model" == "" ]; then
-    model=$(strings /dev/block/bootdevice/by-name/LTALabel | grep -m 1 -oE '(SO-01M|SOV41|901SO|SO-03L|SOV40|802SO)') 2> /dev/null
+    model=$(strings /dev/block/by-name/LTALabel | grep -m 1 -oE '(SO-01M|SOV41|901SO|SO-03L|SOV40|802SO)') 2> /dev/null
 fi
 
 case "$model" in
     "J9110" | "J9150" | "J9180" | "J9210" | "J9260")
-        setprop vendor.radio.multisim.config dsds;;
+        setprop vendor.radio.hardware.sku ds;;
     * )
-        setprop vendor.radio.multisim.config ss;;
+        setprop vendor.radio.hardware.sku ss;;
 esac
 
 if [ "$model" == "" ]; then
