LineageOS/android_device_oneplus_sm8150-common

sm8150-common: Don't use BSG framework for gpt-utils
Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

common.mk
@@ -87,6 +87,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \

Sony-Kumano-Platform-Development/android_device_sony_sm8150-common

common.mk
@@ -218,6 +218,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 