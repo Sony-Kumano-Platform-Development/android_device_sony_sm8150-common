d357f174fd6aa3e091efa5d63e54e51e941732f3
LuK1337 <priv.luk@gmail.com>
2025-07-27 21:39:04 +0200

    sm8150-common: Update WFD vendor blobs from sweet_k6a OS1.0.19.0.THGMIX
    Change-Id: I6f8c4da7384e85ab4a65b7d084bda18381a16b05

--- extract-files.py
+++ extract-files.py
@@ -99,6 +99,9 @@ blob_fixups: blob_fixups_user_type = {
                 'vendor/lib/libsomc_chokoballcmn.so',
         ): blob_fixup()
             .replace_needed('libui.so', 'libui-v34.so'),
+        'vendor/etc/wfdconfig.xml': blob_fixup()
+            .regex_replace('<AudioStreamInSuspend>0</AudioStreamInSuspend>', '<AudioStreamInSuspend>1</AudioStreamInSuspend>')
+            .regex_replace('<HID>0</HID>', '<HID>1</HID>'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {
