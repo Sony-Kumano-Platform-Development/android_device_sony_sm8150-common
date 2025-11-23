a4ed4d5136006facc333ae87d3330335979616b8
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sm8150-common: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -101,6 +101,8 @@ blob_fixups: blob_fixups_user_type = {
             'vendor/lib64/libdpps.so',
         ): blob_fixup()
             .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+        'system_ext/lib64/libwfdnative.so': blob_fixup()
+            .add_needed('libinput_shim.so'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {
