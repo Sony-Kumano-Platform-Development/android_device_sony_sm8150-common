066b463511b632a02f2a346f96dd263d0e7a18bb
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-24 03:08:29 +0800

    sm8150-common: Update some soong config variables to bool type
    Change-Id: Ice4a8e2cc6d756a743116b46299cf833a0e78c57

--- common.mk
+++ common.mk
@@ -176,7 +176,7 @@ PRODUCT_PACKAGES += \
     libvisualizer \
     libvolumelistener
 
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 # Audio Policies
 PRODUCT_COPY_FILES += \
@@ -209,7 +209,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
-$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+$(call soong_config_set_bool,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
