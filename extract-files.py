8a12bbf4ed944fa04033909f86881d4e0dab125b
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm8150-common: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -97,6 +97,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/wfdconfig.xml': blob_fixup()
             .regex_replace('<AudioStreamInSuspend>0</AudioStreamInSuspend>', '<AudioStreamInSuspend>1</AudioStreamInSuspend>')
             .regex_replace('<HID>0</HID>', '<HID>1</HID>'),
+        (
+            'vendor/lib64/libdpps.so',
+        ): blob_fixup()
+            .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 lib_fixups: lib_fixups_user_type = {
